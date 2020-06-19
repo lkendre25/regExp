8858fe51f8af93c91186e4bbfbfba9bff25475ca
lkendre25 <lkendre5652@gmail.com>
2020-06-19 23:43:31 +0530

    email testing

--- regex1.sh
+++ regex1.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-shopt -s extglob
 
-pattern="^[abc]{3}\.[xyz]*[\@]{1}[bl\.co]{5}[\.in]*$"
-username="abc.x@bl.co.in"
+# Sample emails to Test
+shopt -s extglob
+function first_Pattern(){
+pattern="^[abc]+\@[yahoo\.com]{9}$"
+username="bca@yahoo.com"
 
 if [[ $username =~ $pattern ]];then
    echo "Welcome $username"
 else
    echo "Sorry!! "
 fi
+}
+first_Pattern
 
-#!/bin/bash
+function second_Pattern(){
+pattern="^[abc]+\@[yahoo\.com]{9}$"
+username="bca@yahoo.com"
 
+if [[ $username =~ $pattern ]];then
+   echo "Welcome $username"
+else
+   echo "Sorry!! "
+fi
+}
+second_Pattern
