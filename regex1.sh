e2927af86f7865f4289a201978aef496b318b8ae
lkendre25 <lkendre5652@gmail.com>
2020-06-19 20:11:44 +0530

    Pass Validaton

--- regex1.sh
+++ regex1.sh
@@ -2,8 +2,8 @@
 
 shopt -s extglob
 
-pattern="^[A-Z][a-zA-Z]{8,}$"
-pass="Lkglaxmanlk"
+pattern="^([A-Z][0-9])?[\$\#\%][a-zA-Z]{8,}$"
+pass="K0%kglaxmanlk"
 #read pass
 if [[ $pass =~ $pattern ]];then
    echo "Welcome $pass"
